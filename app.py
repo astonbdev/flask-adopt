5e9f454fc9d85ecf8e5c9aafbaf7f247d1031d7e
Brian Aston <astonb.dev@gmail.com>
2022-02-04 15:36:51 -0800

    Finished Part 1

--- app.py
+++ app.py
@@ -27,26 +27,53 @@ toolbar = DebugToolbarExtension(app)
 @app.get('/')
 def show_homepage():
     """Display homepage"""
+
     pets = Pet.query.all()
     return render_template("home.html", pets=pets)
 
 @app.route('/add', methods=["GET", "POST"])
-def add_pet():
+def show_add_pet_form():
+    """displays and processes form POST request for adding a pet"""
 
     pet_form = PetForm()
 
     if pet_form.validate_on_submit():
-        
+
         pet = Pet(
             name=pet_form.name.data,
             species=pet_form.species.data,
             photo_url=pet_form.photo_url.data,
             age=pet_form.age.data,
-            notes=pet_form.notes.data)
+            notes=pet_form.notes.data
+        )
+
         db.session.add(pet)
         db.session.commit()
-        flash(f"Add {pet.name}!")
+        flash(f"Added {pet.name}!")
         return redirect("/")
-    
-    else: 
+
+    else:
         return render_template("add_pet.html", form=pet_form)
+
+@app.route('/<int:pet_id>', methods=["GET", "POST"])
+def show_pet_info_form(pet_id):
+    """displays pet info page, and also processes
+       form POST requests to edit pet info"""
+
+    pet = Pet.query.get_or_404(pet_id)
+    pet_form = PetForm(obj=pet)
+
+    if pet_form.validate_on_submit():
+
+        pet.name = pet_form.name.data
+        pet.species = pet_form.species.data
+        pet.photo_url = pet_form.photo_url.data
+        pet.age = pet_form.age.data
+        pet.notes = pet_form.notes.data
+
+        db.session.commit()
+        flash(f"Edited {pet.name}!")
+        return redirect(f"/{pet_id}")
+
+    else:
+       return render_template("pet_info.html", pet=pet, form=pet_form)
--- forms.py
+++ forms.py
@@ -7,14 +7,16 @@ from wtforms.validators import Optional, InputRequired, URL
 
 def validate_species(form, field):
     """Validates species from defined list, uses wtforms validators"""
+
     species_list = ["cat", "dog", "porcupine"]
-    if field.data not in species_list:
+    if field.data.lower() not in species_list:
         raise ValidationError("Species not valid")
 
 def validate_age(form, field):
     """Validates age from defined list, uses wtforms validators"""
+
     age_list = ["baby", "young", "adult", "senior"]
-    if field.data not in age_list:
+    if field.data.lower() not in age_list:
         raise ValidationError("Age not valid")
 
 class PetForm(FlaskForm):
